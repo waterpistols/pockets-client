014fe94f06de97d2a8548a09bdbdf46ed2f0e04c
vladoros <orosvlad@gmail.com>
2015-11-20 21:31:20 +0200

    Modified header back buttons size & misc mods

--- www/js/app.js
+++ www/js/app.js
@@ -1,104 +1,105 @@
 angular.module('pockets', [
-    'ionic','ionic.service.core',
-    
-    'ionic.service.push',
+        'ionic', 'ionic.service.core',
 
-    'LocalStorageModule',
-    'nvd3ChartDirectives'
-])
-.run(function($ionicPlatform, User, Notification) {
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+        'ionic.service.push',
 
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
+        'LocalStorageModule',
+        'nvd3ChartDirectives'
+    ])
+    .run(function($ionicPlatform, User, Notification) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-        User.sync();
-        Notification.register();
-    });
-})
-.config(function($stateProvider, $ionicAppProvider, $urlRouterProvider, Config) {
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
 
-    $ionicAppProvider.identify({
-        app_id: Config.ionic.app_id,
-        api_key: Config.ionic.api_key,
-        dev_push: Config.ionic.dev_push
-    });
+            User.sync();
+            Notification.register();
+        });
+    })
+    .config(function($stateProvider, $ionicAppProvider, $urlRouterProvider, $animateProvider, Config) {
+        $animateProvider.classNameFilter(/^((?!col-md).)*$/);
 
-    $stateProvider
-        .state('tab', {
-            url: '/tab',
-            abstract: true,
-            templateUrl: 'templates/tabs.html'
-        })
-        .state('login', {
-            url: '/login',
-            templateUrl: 'js/login/login.html',
-            controller: 'LoginCtrl'
-        })
-        .state('tab.cashboard', {
-            url: '/cashboard',
-            views: {
-                'tab-cashboard': {
-                    templateUrl: 'js/cashboard/cashboard.html',
-                    controller: 'CashboardCtrl'
-                }
-            }
-        })
-        .state('tab.pockets', {
-            url: '/pockets',
-            views: {
-                'tab-pockets': {
-                    templateUrl: 'js/pockets/pockets.html',
-                    controller: 'PocketsCtrl'
-                }
-            },
-            resolve: {
-                pocket: function(Pocket) {
-                    return true;
-                }
-            }
-        })
-        .state('tab.pocket-details', {
-            url: '/pocket/:pocketId',
-            views: {
-                'tab-pockets': {
-                    templateUrl: 'js/pockets/pocket-details.html',
-                    controller: 'PocketDetailsCtrl'
-                }
-            }
-        })
-        .state('tab.history', {
-            url: '/history',
-            views: {
-                'tab-history': {
-                    templateUrl: 'js/history/history.html',
-                    controller: 'HistoryCtrl'
-                }
-            },
-            resolve: {
-                history: function(History) {
-                    return History.getHistory();
-                }
-            }
-        })
-        .state('tab.settings', {
-            url: '/settings',
-            views: {
-                'tab-settings': {
-                    templateUrl: 'js/settings/settings.html',
-                    controller: 'SettingsCtrl'
-                }
-            }
+        $ionicAppProvider.identify({
+            app_id: Config.ionic.app_id,
+            api_key: Config.ionic.api_key,
+            dev_push: Config.ionic.dev_push
         });
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/cashboard');
-});
+        $stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'js/login/login.html',
+                controller: 'LoginCtrl'
+            })
+            .state('tab.cashboard', {
+                url: '/cashboard',
+                views: {
+                    'tab-cashboard': {
+                        templateUrl: 'js/cashboard/cashboard.html',
+                        controller: 'CashboardCtrl'
+                    }
+                }
+            })
+            .state('tab.pockets', {
+                url: '/pockets',
+                views: {
+                    'tab-pockets': {
+                        templateUrl: 'js/pockets/pockets.html',
+                        controller: 'PocketsCtrl'
+                    }
+                },
+                resolve: {
+                    pocket: function(Pocket) {
+                        return true;
+                    }
+                }
+            })
+            .state('tab.pocket-details', {
+                url: '/pocket/:pocketId',
+                views: {
+                    'tab-pockets': {
+                        templateUrl: 'js/pockets/pocket-details.html',
+                        controller: 'PocketDetailsCtrl'
+                    }
+                }
+            })
+            .state('tab.history', {
+                url: '/history',
+                views: {
+                    'tab-history': {
+                        templateUrl: 'js/history/history.html',
+                        controller: 'HistoryCtrl'
+                    }
+                },
+                resolve: {
+                    history: function(History) {
+                        return History.getHistory();
+                    }
+                }
+            })
+            .state('tab.settings', {
+                url: '/settings',
+                views: {
+                    'tab-settings': {
+                        templateUrl: 'js/settings/settings.html',
+                        controller: 'SettingsCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/cashboard');
+    });
