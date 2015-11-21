b9c6dad4810cd36556a2b7756a8e44bc4413d04d
Claudiu Filip <claudiucelfilip@gmail.com>
2015-11-21 03:42:55 +0200

    minor fixes

--- www/js/login/LoginCtrl.js
+++ www/js/login/LoginCtrl.js
@@ -2,7 +2,7 @@
 (function(app) {
     'use strict';
 
-    app.controller("LoginCtrl", function($scope, $state, $ionicPopup, User, Util) {
+    app.controller("LoginCtrl", function($rootScope, $scope, $state, $ionicPopup, User, Util) {
             $scope.data = {};
             $scope.login = function() {
                 if ( ! ($scope.data.user && $scope.data.password) ) {
@@ -12,16 +12,24 @@
                    });
                    return;
                 }
+                $rootScope.loadingClass = true;
                 User.login($scope.data).then(function(success) {
                     if (success) {
                         Util.syncAll();
                         $state.go("tab.cashboard");
                     } else {
+                        $rootScope.loadingClass = false;
                         $ionicPopup.alert({
                            title: 'Error!',
                            template: 'Login failed'
                        });
                     }
+                }, function() {
+                    $rootScope.loadingClass = false;
+                    $ionicPopup.alert({
+                        title: 'Error!',
+                        template: 'Login failed'
+                    });
                 });
             };
         }
